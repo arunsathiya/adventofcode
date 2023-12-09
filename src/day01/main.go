481812f54cf882625532b3bd75be96bef6bc3ae0
Arun <arun@arun.blog>
2023-12-08 16:42:20 -0800

    debugging why regex matches are incorrect with Sharun's test cases: https://github.com/sharunkumar/AdventOfCode/blob/efd7e7adbcefc2ce717cef7a7cfe188e3d52bc10/2023/src/tests/io/day-01-trebuchet.input.2.txt
    Signed-off-by: Arun <arun@arun.blog>

--- src/day01/main.go
+++ src/day01/main.go
@@ -10,28 +10,46 @@ import (
 	"github.com/dlclark/regexp2"
 )
 
-func numbersFromWords(line string) string {
-	numberRegex := regexp2.MustCompile(`(?=(one|two|three|four|five|six|seven|eight|nine|zero))`, regexp2.None)
-	newText, err := numberRegex.ReplaceFunc(line, func(m regexp2.Match) string {
-		replacements := map[string]int{
-			"one":   1,
-			"two":   2,
-			"three": 3,
-			"four":  4,
-			"five":  5,
-			"six":   6,
-			"seven": 7,
-			"eight": 8,
-			"nine":  9,
-			"zero":  0,
-		}
-		return fmt.Sprint(replacements[m.String()])
-	}, -1, -1)
+func numbersFromWords(line string) (string, error) {
+	numberRegex, err := regexp2.Compile(`(?=(one|two|three|four|five|six|seven|eight|nine|zero))`, regexp2.None)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
-	fmt.Println(newText)
-	return newText
+
+	replacements := map[string]string{
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
+		"zero":  "0",
+	}
+
+	match, err := numberRegex.FindStringMatch(line)
+	if err != nil {
+		return "", err
+	}
+
+	offset := 0
+	for match != nil {
+		word := match.GroupByNumber(1).String()
+		if replacement, ok := replacements[word]; ok {
+			before := line[:match.Index+offset]
+			after := line[match.Index+offset+len(word):]
+			line = before + replacement + after
+			offset += len(replacement) - len(word)
+		}
+
+		match, err = numberRegex.FindNextMatch(match)
+		if err != nil {
+			return "", err
+		}
+	}
+	return line, nil
 }
 
 func main() {
@@ -45,7 +63,10 @@ func main() {
 	for scanner.Scan() {
 		line := scanner.Text()
 		calibration := make([]int, 0)
-		line = numbersFromWords(line)
+		line, err = numbersFromWords(line)
+		if err != nil {
+			log.Fatal(err)
+		}
 		for _, v := range line {
 			v, err := strconv.Atoi(string(v))
 			if len(calibration) == 2 {
@@ -57,12 +78,13 @@ func main() {
 		}
 		switch len(calibration) {
 		case 1:
-			sum += calibration[0]*10 + calibration[0]
+			sum += calibration[0] * 11
 		case 2:
 			sum += calibration[0]*10 + calibration[1]
 		default:
 			sum += 0
 		}
+		fmt.Println(calibration)
 	}
 	fmt.Println(sum)
 
