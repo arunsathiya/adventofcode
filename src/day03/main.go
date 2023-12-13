43989a0dec7a7e66a9ae19303b4de37b0f5b06bf
Arun <arun@arun.blog>
2023-12-13 05:06:06 -0800

    Loop through all matches and check if it is near a symbol across all directions: previous line, same line, new line, at above, below, sides, or diagonal positions
    Signed-off-by: Arun <arun@arun.blog>

--- src/day03/main.go
+++ src/day03/main.go
@@ -7,110 +7,7 @@ import (
 	"os"
 	"regexp"
 	"strconv"
-	"strings"
 )
-
-func numbers(input *os.File) (int, error) {
-	re := regexp.MustCompile(`\d+`)
-	lines := make([]string, 0)
-	invalidNumbers := make([]int, 0)
-	sum := 0
-	scanner := bufio.NewScanner(input)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	for _, line := range lines {
-		matchesInLine := re.FindAllString(line, -1)
-		for _, match := range matchesInLine {
-			number, err := strconv.Atoi(match)
-			if err != nil {
-				log.Fatal(err)
-			}
-			sum += number
-		}
-	}
-	for lineIndex, line := range lines {
-		if lineIndex != 0 && lineIndex != len(lines)-1 {
-			matchesInLine := re.FindAllStringIndex(line, -1)
-			for _, match := range matchesInLine {
-				matchIndexAtLineLevelStart := match[0]
-				matchIndexAtLineLevelEnd := match[1] - 1
-				leftOk := (matchIndexAtLineLevelStart == 0 || string(line[matchIndexAtLineLevelStart-1]) == ".")
-				rightOk := (matchIndexAtLineLevelEnd == len(line)-1 || string(line[matchIndexAtLineLevelEnd+1]) == ".")
-				if leftOk && rightOk {
-					for i := matchIndexAtLineLevelStart - 1; i <= matchIndexAtLineLevelEnd+1; i++ {
-						if i >= 0 && i < len(line) {
-							prevLine, nextLine := strings.Split(lines[lineIndex-1], ""), strings.Split(lines[lineIndex+1], "")
-							if i < len(prevLine) && i < len(nextLine) && prevLine[i] == "." && nextLine[i] == "." {
-								invalidNumber, err := strconv.Atoi(line[match[0]:match[1]])
-								if err != nil {
-									log.Fatal(err)
-								}
-								if len(invalidNumbers) == 0 || invalidNumbers[len(invalidNumbers)-1] != invalidNumber {
-									invalidNumbers = append(invalidNumbers, invalidNumber)
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-		if lineIndex == 0 {
-			matchesInLine := re.FindAllStringIndex(line, -1)
-			for _, match := range matchesInLine {
-				matchIndexAtLineLevelStart := match[0]
-				matchIndexAtLineLevelEnd := match[1] - 1
-				leftOk := (matchIndexAtLineLevelStart == 0 || string(line[matchIndexAtLineLevelStart-1]) == ".")
-				rightOk := (matchIndexAtLineLevelEnd == len(line)-1 || string(line[matchIndexAtLineLevelEnd+1]) == ".")
-				if leftOk && rightOk {
-					for i := matchIndexAtLineLevelStart - 1; i <= matchIndexAtLineLevelEnd+1; i++ {
-						if i >= 0 && i < len(line) {
-							nextLine := strings.Split(lines[lineIndex+1], "")
-							if i < len(nextLine) && nextLine[i] == "." {
-								invalidNumber, err := strconv.Atoi(line[match[0]:match[1]])
-								if err != nil {
-									log.Fatal(err)
-								}
-								if len(invalidNumbers) == 0 || invalidNumbers[len(invalidNumbers)-1] != invalidNumber {
-									invalidNumbers = append(invalidNumbers, invalidNumber)
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-		if lineIndex == len(lines)-1 {
-			matchesInLine := re.FindAllStringIndex(line, -1)
-			for _, match := range matchesInLine {
-				matchIndexAtLineLevelStart := match[0]
-				matchIndexAtLineLevelEnd := match[1] - 1
-				leftOk := (matchIndexAtLineLevelStart == 0 || string(line[matchIndexAtLineLevelStart-1]) == ".")
-				rightOk := (matchIndexAtLineLevelEnd == len(line)-1 || string(line[matchIndexAtLineLevelEnd+1]) == ".")
-				if leftOk && rightOk {
-					for i := matchIndexAtLineLevelStart - 1; i <= matchIndexAtLineLevelEnd+1; i++ {
-						if i >= 0 && i < len(line) {
-							prevLine := strings.Split(lines[lineIndex-1], "")
-							if i < len(prevLine) && prevLine[i] == "." {
-								invalidNumber, err := strconv.Atoi(line[match[0]:match[1]])
-								if err != nil {
-									log.Fatal(err)
-								}
-								if len(invalidNumbers) == 0 || invalidNumbers[len(invalidNumbers)-1] != invalidNumber {
-									invalidNumbers = append(invalidNumbers, invalidNumber)
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-	for _, invalidNumber := range invalidNumbers {
-		sum -= invalidNumber
-	}
-	return sum, nil
-}
 
 func main() {
 	input, err := os.Open("input.txt")
@@ -123,4 +20,52 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(sum)
+}
+
+func numbers(input *os.File) (int, error) {
+	re := regexp.MustCompile(`\d+`)
+	lines := make([]string, 0)
+	sum := 0
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	for lineIndex, line := range lines {
+		matchesInLine := re.FindAllStringIndex(line, -1)
+		for _, match := range matchesInLine {
+			number, err := strconv.Atoi(line[match[0]:match[1]])
+			if err != nil {
+				return 0, err
+			}
+			if nearSymbol(lines, lineIndex, match[0], match[1]) {
+				sum += number
+			}
+		}
+	}
+	return sum, nil
+}
+
+func nearSymbol(lines []string, lineIndex, startIndex, endIndex int) bool {
+	directions := []struct{ dx, dy int }{
+		{-1, -1}, {-1, 0}, {-1, 1},
+		{0, -1}, {0, 1},
+		{1, -1}, {1, 0}, {1, 1},
+	}
+	for _, dir := range directions {
+		for col := startIndex; col <= endIndex; col++ {
+			newRow, newCol := lineIndex+dir.dx, col+dir.dy
+			if newRow < 0 || newRow >= len(lines) || newCol < 0 || newCol >= len(lines[newRow]) {
+				continue
+			}
+			char := lines[newRow][newCol]
+			if char != '.' && !isDigit(rune(char)) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func isDigit(ch rune) bool {
+	return ch >= '0' && ch <= '9'
 }
