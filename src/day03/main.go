d1ea5008fd63e2982520567a2678c35575998b70
Arun <arun@arun.blog>
2023-12-14 18:49:27 -0800

    Fixed test
    Signed-off-by: Arun <arun@arun.blog>

--- src/day03/main.go
+++ src/day03/main.go
@@ -25,7 +25,6 @@ func main() {
 func numbers(input *os.File) (int, error) {
 	re := regexp.MustCompile(`\d+`)
 	lines := make([]string, 0)
-	collectedForSum := make([]int, 0)
 	sum := 0
 	scanner := bufio.NewScanner(input)
 	for scanner.Scan() {
@@ -39,12 +38,10 @@ func numbers(input *os.File) (int, error) {
 				return 0, err
 			}
 			if nearSymbol(lines, lineIndex, match[0], match[1]) {
-				collectedForSum = append(collectedForSum, number)
 				sum += number
 			}
 		}
 	}
-	fmt.Println(collectedForSum)
 	return sum, nil
 }
 
@@ -55,7 +52,7 @@ func nearSymbol(lines []string, lineIndex, startIndex, endIndex int) bool {
 		{1, -1}, {1, 0}, {1, 1},
 	}
 	for _, dir := range directions {
-		for col := startIndex; col <= endIndex; col++ {
+		for col := startIndex; col < endIndex; col++ {
 			newRow, newCol := lineIndex+dir.dx, col+dir.dy
 			if newRow < 0 || newRow >= len(lines) || newCol < 0 || newCol >= len(lines[newRow]) {
 				continue
