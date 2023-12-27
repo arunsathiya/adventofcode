34474eb1de934fdb112c44de77c0d777c9c434fc
Arun <arun@arun.blog>
2023-12-27 12:20:58 -0800

    Refactor tests and functions to support both part a and b

--- src/2022/day01/main.go
+++ src/2022/day01/main.go
@@ -9,26 +9,12 @@ import (
 	"strings"
 )
 
-func Day01Of2022(input *os.File) (int, error) {
-	scanner := bufio.NewScanner(input)
-	value := ""
-	for scanner.Scan() {
-		scanned := scanner.Text()
-		value += scanned + "\n"
-	}
-	blocks := strings.Split(value, "\n\n")
+func Day01Of2022PartA(blocks []string) (int, error) {
 	highestSum := 0
-	for _, calories := range blocks {
-		sum := 0
-		calorie := strings.Split(calories, "\n")
-		for i := 0; i < len(calorie); i++ {
-			if calorie[i] != "" {
-				calorie, err := strconv.Atoi(calorie[i])
-				if err != nil {
-					return 0, err
-				}
-				sum += calorie
-			}
+	for _, block := range blocks {
+		sum, err := processBlock(block)
+		if err != nil {
+			return 0, err
 		}
 		if sum > highestSum {
 			highestSum = sum
@@ -37,15 +23,48 @@ func Day01Of2022(input *os.File) (int, error) {
 	return highestSum, nil
 }
 
+func Day01Of2022PartB(blocks []string) (int, error) {
+	return 0, nil
+}
+
+func parseInput(input *os.File) ([]string, error) {
+	scanner := bufio.NewScanner(input)
+	value := ""
+	for scanner.Scan() {
+		scanned := scanner.Text()
+		value += scanned + "\n"
+	}
+	return strings.Split(value, "\n\n"), scanner.Err()
+}
+
+func processBlock(block string) (int, error) {
+	sum := 0
+	calories := strings.Split(block, "\n")
+	for _, calorie := range calories {
+		if calorie != "" {
+			value, err := strconv.Atoi(calorie)
+			if err != nil {
+				return 0, err
+			}
+			sum += value
+		}
+	}
+	return sum, nil
+}
+
 func main() {
 	input, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer input.Close()
-	sum, err := Day01Of2022(input)
+	blocks, err := parseInput(input)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf(err.Error())
 	}
-	fmt.Println(sum)
+	highestSum, err := Day01Of2022PartA(blocks)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	fmt.Println(highestSum)
 }
--- src/2022/day01/main_test.go
+++ src/2022/day01/main_test.go
@@ -7,14 +7,16 @@ import (
 
 func TestDay01Of2022(t *testing.T) {
 	tests := []struct {
-		name     string
-		input    string
-		expected int
+		name      string
+		input     string
+		expectedA int
+		expectedB int
 	}{
 		{
-			name:     "test case from AOC",
-			input:    "test.txt",
-			expected: 24000,
+			name:      "test case from AOC",
+			input:     "test.txt",
+			expectedA: 24000,
+			expectedB: 45000,
 		},
 	}
 	for _, test := range tests {
@@ -24,12 +26,24 @@ func TestDay01Of2022(t *testing.T) {
 				t.Fatalf("Failed to open file %v", err)
 			}
 			defer file.Close()
-			output, err := Day01Of2022(file)
+			blocks, err := parseInput(file)
 			if err != nil {
-				t.Errorf("Expected %d, got %v", test.expected, err)
+				t.Fatalf(err.Error())
 			}
-			if output != test.expected {
-				t.Errorf("Expected %d, got %d", test.expected, output)
+			highestSum, err := Day01Of2022PartA(blocks)
+			if err != nil {
+				t.Errorf("Expected %d, got %v", test.expectedA, err)
+			}
+			if highestSum != test.expectedA {
+				t.Errorf("Expected %d, got %d", test.expectedA, highestSum)
+			}
+			file.Seek(0, 0)
+			sumOfThree, err := Day01Of2022PartB(blocks)
+			if err != nil {
+				t.Errorf("Expected %d, got %v", test.expectedB, err)
+			}
+			if sumOfThree != test.expectedB {
+				t.Errorf("Expected %d, got %d", test.expectedB, highestSum)
 			}
 		})
 	}
