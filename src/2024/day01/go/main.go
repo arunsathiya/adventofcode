a7de15ec1578f1ce5a162d13b7f88a56315f7bae
Arun <arun@arun.blog>
2024-12-22 06:08:53 +0530

    Solve 2024 day 1 part A

---
+++ src/2024/day01/go/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func calculateDistances(input *os.File) (int, error) {
+	scanner := bufio.NewScanner(input)
+	sum := 0
+	a := make([]int, 0)
+	b := make([]int, 0)
+	for scanner.Scan() {
+		line := scanner.Text()
+		first := strings.Split(line, "   ")[0]
+		firstN, err := strconv.Atoi(first)
+		if err != nil {
+			log.Fatal(err)
+		}
+		second := strings.Split(line, "   ")[1]
+		secondN, err := strconv.Atoi(second)
+		if err != nil {
+			log.Fatal(err)
+		}
+		a = append(a, firstN)
+		b = append(b, secondN)
+	}
+	sort.Slice(a, func(i, j int) bool {
+		return a[i] < a[j]
+	})
+	sort.Slice(b, func(i, j int) bool {
+		return b[i] < b[j]
+	})
+	for i := 0; i < len(a); i++ {
+		diff := math.Abs(float64(a[i] - b[i]))
+		sum += int(diff)
+	}
+	return sum, nil
+}
+
+func main() {
+	input, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer input.Close()
+	sum, err := calculateDistances(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(sum)
+}
